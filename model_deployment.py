8261d9380130b964bbe0e58dc37b5b8770fd47ee
aravind22322k <aravindsg072@gmail.com>
2024-08-11 11:00:57 +0530

    Update model_deployment.py

--- model_deployment.py
+++ model_deployment.py
@@ -1,16 +1,6 @@
 import sagemaker
-from sagemaker import Session
 
-# Set the region
-region = 'us-west-2'  # Replace with your desired AWS region
-
-# Initialize the SageMaker session with the specified region
-sagemaker_session = Session(boto_session=sagemaker.Session(region_name=region))
-
-# Rest of your deployment code
-xgb_model = ...  # Your model definition or loading
 xgb_predictor = xgb_model.deploy(
     initial_instance_count=1,
-    instance_type='ml.m4.xlarge',
-    sagemaker_session=sagemaker_session
+    instance_type='ml.m4.xlarge'
 )
